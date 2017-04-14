523f344f20ea85996044914e816c165fda2aaf36
saif99ss <saifsalah66@yahoo.com>
2017-04-14 03:21:25 -0500

    application is ready

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'acts_as_votable', '~> 0.10.0'
+gem 'masonry-rails', '~> 0.2.4'
 gem 'devise', '~> 4.2', '>= 4.2.1'
 gem 'paperclip', '~> 5.1'
 gem 'haml', '~> 4.0', '>= 4.0.7'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
-before_action :find_pin, only: [:show, :edit, :update, :destroy]
+before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+before_action :authenticate_user!, except: [:index, :show]
 
 
 
@@ -44,6 +45,14 @@ end
 		@pin.destroy
 		redirect_to root_path
 	end
+
+
+def upvote
+	@pin.upvote_by current_user
+	redirect_to :back
+
+end
+
 
 private 
 
